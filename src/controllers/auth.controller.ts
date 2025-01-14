46b0bf8081297d4223c7f63085c986c9a8f84d8f
Mateie <contact@mateie.dev>
2025-01-14 13:25:01 -0800

    added login route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,13 +7,13 @@ import {
     type Response,
 } from "express";
 import UserModel from "models/User";
-import { validateSignup } from "validation/auth.validation";
+import { validateLogin, validateSignup } from "validation/auth.validation";
 import { ZodError } from "zod";
 
 import bcrypt from "bcrypt";
 import crypto from "crypto";
 import Cryptr from "cryptr";
-import { encrypt } from "structures/Crypt";
+import { decrypt, encrypt } from "structures/Crypt";
 import { genSnowflake } from "structures/Util";
 
 export class AuthController {
@@ -22,6 +22,7 @@ export class AuthController {
 
     constructor() {
         this.router.post(`${this.path}/signup`, this.signup);
+        this.router.post(`${this.path}/login`, this.login);
     }
 
     async signup(req: Request, res: Response, next: NextFunction) {
@@ -72,6 +73,75 @@ export class AuthController {
 
             res.status(HTTP_RESPONSE_CODE.CREATED).json({
                 success: true,
+            });
+        } catch (err) {
+            let error = err;
+
+            if (err instanceof HttpException)
+                error = new HttpException(
+                    HTTP_RESPONSE_CODE.BAD_REQUEST,
+                    err.message,
+                    err.errors
+                );
+
+            if (err instanceof ZodError) {
+                const errors = err.errors.map((error) => ({
+                    path: error.path[0].toString(),
+                    message: error.message,
+                }));
+
+                error = new HttpException(
+                    HTTP_RESPONSE_CODE.BAD_REQUEST,
+                    "Invalid request data",
+                    errors
+                );
+            }
+
+            next(error);
+        }
+    }
+
+    async login(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { username, email, password } = validateLogin.parse(req.body);
+
+            const user = await UserModel.findOne({
+                $or: [{ username }, { email }],
+            });
+
+            if (!user)
+                throw new HttpException(
+                    HTTP_RESPONSE_CODE.BAD_REQUEST,
+                    "Invalid credentials",
+                    [
+                        {
+                            path: "password",
+                            message: "Invalid username or password",
+                        },
+                    ]
+                );
+
+            const decrypted = decrypt(user.password);
+            const pass = bcrypt.compareSync(
+                password,
+                new Cryptr(user.privateKey).decrypt(decrypted)
+            );
+
+            if (!pass)
+                throw new HttpException(
+                    HTTP_RESPONSE_CODE.BAD_REQUEST,
+                    "Invalid credentials",
+                    [
+                        {
+                            path: "password",
+                            message: "Invalid username or password",
+                        },
+                    ]
+                );
+
+            res.status(HTTP_RESPONSE_CODE.SUCCESS).json({
+                token: encrypt(user.generateToken()),
+                ...user.toJSON(),
             });
         } catch (err) {
             let error = err;
