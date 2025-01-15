7ddfc582a677e750995f91ed4cfde76e7b6b2866
Mateie <contact@mateie.dev>
2025-01-15 13:26:07 -0800

    fixed imported type

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,10 +1,9 @@
-import type { UserWithSensitiveData } from "@mutualzz/types";
+import type { User } from "@mutualzz/types";
 import { HttpException } from "exceptions/HttpException";
 import type { NextFunction, Request, Response } from "express";
 
 import jwt from "jsonwebtoken";
 
-import logger from "Logger";
 import { decrypt } from "structures/Crypt";
 
 const { JWT_SECRET } = process.env;
@@ -15,15 +14,11 @@ const authMiddleware = (req: Request, _: Response, next: NextFunction) => {
         if (!req.headers.authorization) return next();
         const token = req.headers.authorization?.split(" ")[1] ?? null;
         if (!token) throw new HttpException(401, "Unauthorized");
-        const user = jwt.verify(
-            decrypt(token),
-            JWT_SECRET
-        ) as UserWithSensitiveData;
+        const user = jwt.verify(decrypt(token), JWT_SECRET) as User;
         req.user = user;
 
         next();
     } catch (err) {
-        logger.error(err);
         next(err);
     }
 };
