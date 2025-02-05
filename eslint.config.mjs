556f3226465195a3b6000f3b518f73b1d074b552
mateie <mateie@gmail.com>
2025-02-05 09:17:04 -0800

    import eslint typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,20 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import sonarjs from "eslint-plugin-sonarjs";
+import importPlugin from "eslint-plugin-import";
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommended,
     tseslint.configs.stylistic,
     sonarjs.configs.recommended,
+    {
+        files: ["**/*.{ts}"],
+        extends: [
+            importPlugin.flatConfigs.recommended,
+            importPlugin.flatConfigs.typescript,
+        ],
+    },
     {
         rules: {
             "@typescript-eslint/no-unused-vars": [
