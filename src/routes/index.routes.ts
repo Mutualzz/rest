6967b681133bc14d86cb6b3c95af8a7633dae647
Mateie <contact@mateie.dev>
2025-04-05 01:35:11 -0700

    added ack route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,8 +2,6 @@ import MainController from "controllers/index.controller";
 import express from "express";
 const router = express.Router();
 
-const path = "/";
-
-router.get(`${path}ack`, (...args) => MainController.ack(...args));
+router.get(`/ack`, (...args) => MainController.ack(...args));
 
 export default router;
